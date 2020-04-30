7492927733b55f4313977100e996d8e9c4cbafac
Kimberley Tham <kimbertham@gmail.com>
2020-04-30 09:56:07 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import axios from 'axios'
+ 
+class App extends React.Component {
+state= {
+  stations: []
+}
 
-const App = () => {
+async componentDidMount() {
+  const res = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=a6f34e87c9902a29316fd7cff8f50328&language=en-US&page=15')
+  
+  console.log(res)
+}
+
+
+
+
+
+
+render(){
+
   return (
-    <h1>Hello World</h1>
+    <h1> idk whats happening</h1>
   )
 }
+}
+
 
 export default App
